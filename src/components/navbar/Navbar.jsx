94aca3ded2807e7da35a6b0071ea69d4b5dbfc85
Ravleen-192 <meravleen@gmail.com>
2022-08-25 17:48:29 +0530

    small screen menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -31,11 +31,10 @@ const Navbar = () => {
         {toggleMenu && (
         <div className="zm__navbar-menu_container scale-up-center">
           <div className="zm__navbar-menu_container-links">
-            <p><a href="#home">Home</a></p>
-            <p><a href="#wzm">What is ZetaMesh?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wzm">Our Data Offerings</a></p>
+          <p><a href="#possibility">Engagement  Models</a></p>
+          <p><a href="#features">Past Performance</a></p>
           </div>
           <div className="zm__navbar-menu_container-links-sign">
             <p>Sign in</p>
